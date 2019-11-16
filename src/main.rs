cad772bd6ebc4ee19bfcfb638e09bd2f4c49a5ca
akaishi <galigalikun@gmail.com>
2019-11-16 13:53:42 +0900

    rand add

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
     println!("Hello, world!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {} , {}", guess, secret_number);
 }
